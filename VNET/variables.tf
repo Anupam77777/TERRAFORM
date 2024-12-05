f319a1a8b12472d658550fea67e734cf4352635a
Anupam kundu <kundu.anupam007@gmail.com>
2024-12-05 19:35:27 +0000

    Added 3 files to /VNET

---
+++ VNET/variables.tf
@@ -0,0 +1,85 @@
+variable "bkstrgrg" {
+  type        = string
+  description = "The name of the backend storage account resource group"
+  default     = "MYFIRSTRG"
+}
+
+variable "bkstrg" {
+  type        = string
+  description = "The name of the backend storage account"
+  default     = "backendstorageterra"
+}
+
+variable "bkcontainer" {
+  type = string
+  description = "The container name for the backend config"
+  default = "tfstate"
+}
+
+variable "bkstrgkey" {
+  type = string
+  description = "The access key for the storage account"
+  default = "devpipline.terraform.tfstate"
+}
+
+variable "resourcegroup_name" {
+  type        = string
+  description = "The name of the resource group"
+  default     = "vnetrg"
+}
+
+variable "location" {
+  type        = string
+  description = "The region for the deployment"
+  default     = "eastus"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags used for the deployment"
+  default = {
+    "Environment" = "Lab"
+    "Owner"       = "Anupam"
+  }
+}
+
+variable "vnet_name" {
+  type        = string
+  description = "The name of the vnet"
+  default     = "Myfirstvnet"
+}
+
+variable "vnet_address_space" {
+  type        = list(any)
+  description = "the address space of the VNet"
+  default     = ["10.211.0.0/16"]
+}
+
+variable "subnets" {
+  type = map(any)
+  default = {
+    subnet_1 = {
+      name             = "subnet_1"
+      address_prefixes = ["10.211.1.0/24"]
+    }
+    subnet_2 = {
+      name             = "subnet_2"
+      address_prefixes = ["10.211.2.0/24"]
+    }
+    subnet_3 = {
+      name             = "subnet_3"
+      address_prefixes = ["10.211.3.0/24"]
+    }
+    # The name must be AzureBastionSubnet
+    bastion_subnet = {
+      name             = "AzureBastionSubnet"
+      address_prefixes = ["10.211.250.0/24"]
+    }
+  }
+}
+
+variable "bastionhost_name" {
+  type        = string
+  description = "The name of the basion host"
+  default     = "BastionHost"
+}
